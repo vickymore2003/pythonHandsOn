c936efc43273d150fe906a3d119c755272d8522d
Sunny More <vickymore2003@gmail.com>
2024-02-06 21:52:26 +0530

    Day 14 complete

---
+++ Day14/function_2_add.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    print(a+b)
+
+def addUserInput(a,b):
+    a=int(input("Enter first number: "))
+    b=int(input("Enter secound number: "))
+    sum=a+b
+    print(sum) 
+
+add(3,8)
+
+addUserInput(a=any,b=any)
