6935b4abaa821132a5713ea8c5763d581e536547
vickymore2003 <47276311+vickymore2003@users.noreply.github.com>
2024-02-18 16:22:55 +0530

    Day 18 COmplete (#11)
    Co-authored-by: Sunny More <sunny.more@ansys.com>

---
+++ Day18/Array_01.py
@@ -0,0 +1,11 @@
+#Array is special variable which is used to store multiple values
+#a=["pune","munbai","delhi","chennai"]
+# to access any var use index  
+
+a=["pune","munbai","delhi","chennai","banglore"]
+a.append("nagpur")
+a.remove("delhi")
+for x in a:
+    print(x )
+print(a[2])
+print(len(a))
