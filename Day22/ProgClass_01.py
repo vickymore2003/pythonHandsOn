50a631fff5ce4b42c0b11b68efe802389e60c416
Sunny More <sunny.more@ansys.com>
2024-03-03 14:43:02 +0530

    Day22

---
+++ Day22/ProgClass_01.py
@@ -0,0 +1,15 @@
+# Program using classes
+
+class Employee:
+    def __init__(self,name,address,age,salary):
+        self.name=name
+        self.age=age
+        self.address=address
+        self.salary=salary
+
+def show(self):
+    print("Name= ",self.name)
+    print("age= ",self.age)
+    print("address= ",self.address)
+    print("salary= ",self.salary)
+    da=
